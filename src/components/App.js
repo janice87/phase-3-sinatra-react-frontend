165b9b9a8b4f22409afb8815e48d7059507f6c40
Janice Chan <janice.chan1288@gmail.com>
2022-07-22 10:36:38 -0400

    set up browser paths

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Switch>
+       
+        <Route path=""></Route>
+        <Route path=""></Route>
+      
+
+      </Switch>
+      
+    
+    </div>
+  );
+}
+
+export default App;
