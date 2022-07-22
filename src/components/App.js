b9e2d8b782d40d075af7d15f4cce8e4a6d86d853
Janice Chan <janice.chan1288@gmail.com>
2022-07-22 13:40:34 -0400

    update containers

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
-
+import Navbar from './Navbar'
+import Home from './Home'
+import TodoContainer from './TodoContainer'
+import MoodContainer from './MoodContainer'
+import JournalContainer from './JournalContainer'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Switch>
-       
+        <Route exact path="/"><Home /></Route>
+        <Route path="/todos"><TodoContainer /></Route>
+        <Route path="/moods"><MoodContainer /></Route>
+        <Route path="/journals"><JournalContainer /></Route>
         <Route path=""></Route>
         <Route path=""></Route>
       
