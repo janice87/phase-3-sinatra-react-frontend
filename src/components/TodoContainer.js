8a607b0965c37e4f42b76842f54cee9d01bfadb3
Janice Chan <janice.chan1288@gmail.com>
2022-07-22 12:51:22 -0400

    add handle add and handle delete functions

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import {useState, useEffect} from 'react'
 import TodoList from './TodoList'
+import TodoForm from './TodoForm';
 
 const TodoContainer = () => {
     const [todos, setTodos] = useState([])
-
+    
     useEffect(() => {
         fetch('http://localhost:9292/todos')
         .then(res => res.json())
         .then(data => setTodos(data))
     }, [])
 
+    const handleAddTodo = (newTodo) => {
+      const updatedTodos = [...todos, newTodo]
+      setTodos(updatedTodos)
+    }
+
+    const handleDeleteTodo = (id) => {
+      const updatedTodos = todos.filter(todo => todo.id !== id)
+      setTodos(updatedTodos)
+    }
+
     return (
-      <div>       
-        <TodoList todos={todos} />
+      <div>   
+        <TodoForm onHandleAddTodo={handleAddTodo} />    
+        <TodoList todos={todos} onHandleDeleteTodo={handleDeleteTodo} />
       
       </div>
     );
