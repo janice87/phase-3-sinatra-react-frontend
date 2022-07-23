e84e23e3453918914fb98a70d8e351243ec954de
Janice Chan <janice.chan1288@gmail.com>
2022-07-23 10:33:19 -0400

    passed in  delete and edit props

--- src/components/JournalDetails.js
+++ src/components/JournalDetails.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
-const JournalDetails = ({journalObj}) => {
-    const {date, content} = journalObj
+const JournalDetails = ({journalObj, onDeleteJournal, onUpdateJournal}) => {
+const {date, content, id} = journalObj
+
+const handleUpdate = () => {
+      fetch(`http://localhost:9292/journals/${id}`, {
+        method: 'PATCH', 
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          date: date,
+          content: content
+        })
+      })
+      .then(res => res.json())
+      .then((updatedJournal) => onUpdateJournal(updatedJournal))
+    }
+
+const handleDelete = () => {
+  fetch(`http://localhost:9292/journals/${id}`, {
+    method: 'DELETE'
+  })
+  .then(res => res.json())
+  .then(() => onDeleteJournal(id))
+}
+
     return (
       <div>
-        <p>{date}</p> 
+        <h3>{date}</h3> 
         <p>{content}</p>
+        <button onClick={handleUpdate}>Edit</button>
+        <button onClick={handleDelete}>Delete</button>
       </div>
     );
   }
