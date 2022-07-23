9da3e1c806280661bbcc3deec9e9eb4235513cb8
Janice Chan <janice.chan1288@gmail.com>
2022-07-23 14:58:15 -0400

    fixed reactdom render

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import ReactDOM from 'react-dom';
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
     <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// new code
+//import ReactDOM from 'react-dom/client';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Router>
+//     <App />
+//     </Router>
+//   </React.StrictMode>
+// );
+
