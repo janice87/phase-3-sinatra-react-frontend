fd03a1cb8bfb3e10985546670e969fbf826aa31e
Janice Chan <janice.chan1288@gmail.com>
2022-07-22 21:51:33 -0400

    add add journal function

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Navbar from './Navbar'
 import Home from './Home'
 import TodoContainer from './TodoContainer'
 import MoodContainer from './MoodContainer'
 import JournalContainer from './JournalContainer'
+import JournalForm from './JournalForm'
 
 const App = () => {
+
+  const [journals, setJournals] = useState([])
+
+  useEffect(() => {
+      fetch('http://localhost:9292/journals')
+      .then(res => res.json())
+      .then(data => setJournals(data))
+  }, [])
+
+  const handleAddJournal = (newJournal) => {
+    const updatedJournals = [...journals, newJournal]
+    setJournals(updatedJournals)
+  }
+
   return (
     <div>
       <Navbar />
@@ -14,14 +30,11 @@ const App = () => {
         <Route exact path="/"><Home /></Route>
         <Route path="/todos"><TodoContainer /></Route>
         <Route path="/moods"><MoodContainer /></Route>
-        <Route path="/journals"><JournalContainer /></Route>
-        <Route path=""></Route>
-        <Route path=""></Route>
-      
+        <Route path="/journals/new"><JournalForm onHandleAddJournal={handleAddJournal} /></Route>
+        <Route path="/journals"><JournalContainer journals={journals} /></Route>
+        <Route path=""></Route>     
 
-      </Switch>
-      
-    
+      </Switch>          
     </div>
   );
 }
