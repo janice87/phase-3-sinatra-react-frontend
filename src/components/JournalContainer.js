919cedaa3e2ae1d06497a216ffda1e4697e4ac31
Janice Chan <janice.chan1288@gmail.com>
2022-07-22 21:52:04 -0400

    pass in journals props

--- src/components/JournalContainer.js
+++ src/components/JournalContainer.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {useState, useEffect} from 'react'
+// import {useState, useEffect} from 'react'
 import JournalList from './JournalList'
 
-const JournalContainer = () => {
-    const [journals, setJournals] = useState([])
+const JournalContainer = ({journals}) => {
+    // const [journals, setJournals] = useState([])
 
-    useEffect(() => {
-        fetch('http://localhost:9292/journals')
-        .then(res => res.json())
-        .then(data => setJournals(data))
-    }, [])
+    // useEffect(() => {
+    //     fetch('http://localhost:9292/journals')
+    //     .then(res => res.json())
+    //     .then(data => setJournals(data))
+    // }, [])
 
     return (
       <div>       
