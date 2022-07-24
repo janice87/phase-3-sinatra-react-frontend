ca1be8aaf95cc6cad463dbc184f1606e3ab828fc
Janice Chan <janice.chan1288@gmail.com>
2022-07-24 14:21:38 -0400

    add link to edit form

--- src/components/JournalDetails.js
+++ src/components/JournalDetails.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
 
 const JournalDetails = ({journalObj, onDeleteJournal}) => {
 const {date, content, id} = journalObj
-const history = useHistory();
+// const history = useHistory();
 
 // const handleUpdate = () => {
 //       fetch(`http://localhost:9292/journals/${id}`, {
@@ -31,9 +33,10 @@ const handleDelete = () => {
   .then(() => onDeleteJournal(id))
 }
 
-const handleEditForm = () => {
-  history.push(`/journals/${id}`)
-}
+// const handleEditForm = () => {
+//   history.push(`/journals/${id}`)
+// }
+
 
     return (
       <div>
@@ -48,8 +51,9 @@ const handleEditForm = () => {
         <h3>{date}</h3> 
         <p>{content}</p>
       
-        <button onClick={handleEditForm}>Edit</button>
-        <button onClick={handleDelete}>Delete</button>
+        {/* <button onClick={handleEditForm}>Edit</button> */}
+        <Link to={`/journals/${id}/edit`}><EditOutlinedIcon /></Link>
+        <button onClick={handleDelete}><DeleteForeverOutlinedIcon /></button>
         </Box>
         </Container>
       </div>
