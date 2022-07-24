f4a42109fa9cbe2954c5937a257615989ab6b6a0
Janice Chan <janice.chan1288@gmail.com>
2022-07-23 22:59:27 -0400

    create todo edit

---
+++ src/components/TodoEditForm.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const TodoEditForm = () => {
+    const [todoObj, setTodoObj] = useState("")
+    const {id} = useParams(); 
+// does this make sense???
+    useEffect(() => {
+        fetch(`http://localhost:9292/todos/${id}`)
+        .then(res => res.json())
+        .then(todoObj => setTodoObj(todoObj))
+    }, [id])
+
+    return (
+      <div>
+        <p></p>     
+      
+      </div>
+    );
+  }
+  
+  export default TodoEditForm;
