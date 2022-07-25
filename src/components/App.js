2d44d631aa54ce66bd1b2e348d470a5d32168acf
Janice Chan <janice.chan1288@gmail.com>
2022-07-24 21:05:18 -0400

    update route

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import {Route, Switch} from 'react-router-dom'
 import Navbar from './Navbar'
 import Home from './Home'
 import TodoContainer from './TodoContainer'
+import TodoForm from './TodoForm'
 import MoodContainer from './MoodContainer'
 import JournalContainer from './JournalContainer'
 import JournalForm from './JournalForm'
 import JournalEditForm from './JournalEditForm'
-
 
 const App = () => {
 
@@ -43,7 +43,8 @@ const App = () => {
       <Switch>
         <Route exact path="/"><Home /></Route>
         <Route exact path="/todos"><TodoContainer /></Route>
-        <Route exact path="/todos/:id"><TodoContainer /></Route>
+        <Route exact path="/todos/new"><TodoForm /></Route>
+        {/* <Route exact path="/todos/:id/edit"><TodoEditForm /></Route> */}
         <Route exact path="/moods"><MoodContainer /></Route>
         <Route exact path="/journals/new"><JournalForm onHandleAddJournal={handleAddJournal} /></Route>
         <Route exact path="/journals/:id/edit"><JournalEditForm journals={journals} onUpdateJournal={handleUpdateJournal} /></Route>     
